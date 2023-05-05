94596d72e904fbb998c139e6db62537835e775ba
nora-sch <nora.schumann.development@gmail.com>
2023-05-05 10:06:01 +0200

    add Fighter class

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const {Fighter} = require('./src/Fighter.js');
+
 // First Labour : Heracles vs Nemean Lion
 // use your Figher class here
+
+// bearded_person Heracles, strength 20, dexterity 6
+// lion Nemean Lion, strength 11, dexterity 13
+
+const heracles = new Fighter('🧔 Heracles', 20, 6);
+const lion = new Fighter('🦁 Nemean Lion', 11 , 13);
+console.log(heracles.getStrength());
+console.log(lion.getName());
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,1 +1,43 @@
 /* Fighter class definition */
+
+const MAX_LIFE = 100;
+
+class Fighter {
+  #name;
+  #strength;
+  #dexterity;
+  #life;
+  constructor(name, strength, dexterity, life = MAX_LIFE) {
+    this.#name = name;
+    this.#strength = strength;
+    this.#dexterity = dexterity;
+    this.#life = life;
+  }
+
+  getName() {
+    return this.#name;
+  }
+  setName(name) {
+    this.#name = name;
+  }
+  getStrength() {
+    return this.#strength;
+  }
+  setStrength(strength) {
+    this.#strength = strength;
+  }
+  getDexterity() {
+    return this.#dexterity;
+  }
+  setDexterity(dexterity) {
+    this.#dexterity = dexterity;
+  }
+  getLife() {
+    return this.#life;
+  }
+  setName(life) {
+    this.#life = life;
+  }
+}
+
+module.exports = {Fighter}
